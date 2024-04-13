51dbe369e4ba51d254d92db87d085c31463f7fb2
trandangquoc271202 <20130376@st.hcmuaf.edu.vn>
2024-04-14 00:52:00 +0700

    upload UI section banner

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,6 +1,67 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <>
+            <section className="home-wrapper-1 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="main-banner position-relative p-3">
+                                <img src="images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner"/>
+                                <div className="main-banner-content position-absolute">
+                                    <h4>SIÊU GIẢM GIÁ.</h4>
+                                    <h5>Ipad S13+ Pro.</h5>
+                                    <p>Từ 23 triệu hoặc 1 triệu/tháng.</p>
+                                    <Link className="button">MUA NGAY</Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="d-flex flex-wrap gap-10 justify-content-center align-items-center">
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-01.jpg" className="img-fluid rounded-3"
+                                         alt="main banner"/>
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>SIÊU GIẢM GIÁ.</h4>
+                                        <h5>Ipad S13+ Pro.</h5>
+                                        <p>Từ 23 triệu<br/> hoặc 1 triệu/tháng.</p>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-02.jpg" className="img-fluid rounded-3"
+                                         alt="main banner"/>
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>SIÊU GIẢM GIÁ.</h4>
+                                        <h5>Ipad S13+ Pro.</h5>
+                                        <p>Từ 23 triệu<br/> hoặc 1 triệu/tháng.</p>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-03.jpg" className="img-fluid rounded-3"
+                                         alt="main banner"/>
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>SIÊU GIẢM GIÁ.</h4>
+                                        <h5>Ipad S13+ Pro.</h5>
+                                        <p>Từ 23 triệu<br/> hoặc 1 triệu/tháng.</p>
+                                    </div>
+                                </div>
+                                <div className="small-banner position-relative">
+                                    <img src="images/catbanner-04.jpg" className="img-fluid rounded-3"
+                                         alt="main banner"/>
+                                    <div className="small-banner-content position-absolute">
+                                        <h4>SIÊU GIẢM GIÁ.</h4>
+                                        <h5>Ipad S13+ Pro.</h5>
+                                        <p>Từ 23 triệu<br/> hoặc 1 triệu/tháng.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 export default Home;
