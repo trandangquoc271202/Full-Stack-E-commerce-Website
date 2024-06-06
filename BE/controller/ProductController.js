4ca0bf1be6068a787f673c0029d93e2d12d3bc6c
trandangquoc271202 <20130376@st.hcmuaf.edu.vn>
2024-06-06 22:54:00 +0700

    create function api findById and updateProduct for productController

--- BE/controller/ProductController.js
+++ BE/controller/ProductController.js
@@ -1,6 +1,7 @@
 const Product = require("../model/productModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
+const validationMongoId = require("../validation/validationMongoId");
 const createProduct = asyncHandler(async (req, res) => {
     try {
         if (req.body.title) {
@@ -12,6 +13,7 @@ const createProduct = asyncHandler(async (req, res) => {
         throw new Error(error);
     }
 });
+
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
         // Filtering
@@ -57,7 +59,44 @@ const getAllProduct = asyncHandler(async (req, res) => {
         throw new Error(error);
     }
 });
+
+const updateProduct = asyncHandler(async (req, res, next) => {
+    const {id} = req.params; // Lấy giá trị id từ req.params
+    validationMongoId(id); // Gọi hàm validateMongoDbId để kiểm tra tính hợp lệ của id
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title);
+        }
+        const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+            new: true, // Trả về tài liệu đã cập nhật thay vì tài liệu gốc
+            runValidators: true, // Chạy validators trên tài liệu được cập nhật
+        });
+
+        if (!updatedProduct) {
+            return res.status(404).json({message: "Product not found"});
+        } else {
+            return res.json(updatedProduct);
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+const findById = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    validationMongoId(id);
+    try {
+        const product = await Product.findById({_id: id});
+        res.json(product);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({message: 'Server error'});
+    }
+});
+
 module.exports = {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    updateProduct,
+    findById
 };
--- BE/routes/ProductRouter.js
+++ BE/routes/ProductRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    updateProduct,
+    findById
 } = require("../controller/ProductController");
 // const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getAllProduct);
+router.put("/:id", updateProduct);
+router.get("/:id", findById);
 module.exports = router;
