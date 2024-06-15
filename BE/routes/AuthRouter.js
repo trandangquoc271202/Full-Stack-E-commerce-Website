828239602c9609e51c5c0141dccc6c2b7627ce12
trandangquoc271202 <20130376@st.hcmuaf.edu.vn>
2024-06-16 03:23:09 +0700

    create api order with create, updatestate, getall, getbyIdUser, getOrder

--- BE/routes/AuthRouter.js
+++ BE/routes/AuthRouter.js
@@ -6,7 +6,12 @@ const {
     getUserCart,
     deleteProductFromCart,
     updateQuantityCart,
-    login
+    login,
+    createOrder,
+    getOrders,
+    updateOrderStatus,
+    getAllOrders,
+    getOrderByUserId
 } = require("../controller/UserController");
 const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -14,6 +19,13 @@ router.post("/register", createUser);
 router.post("/login", login);
 router.get("/favorite",authMiddleware, getFavorite);
 router.post("/cart",authMiddleware, userCart);
+router.post("/cart/cash-order", authMiddleware, createOrder);
+router.get("/get-orders", authMiddleware, getOrders);
+
+router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
+router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getOrderByUserId);
+router.put("/order/update-order/:id", authMiddleware, isAdmin, updateOrderStatus);
+
 router.get("/cart",authMiddleware, getUserCart);
 router.delete('/cart/:id',authMiddleware, deleteProductFromCart);
 router.put('/cart', authMiddleware, updateQuantityCart);
